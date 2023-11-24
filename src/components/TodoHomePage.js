2da01ecc22b8300d68b51ce8dc6a3217e29acef5
Afsal <afsalpx@gmail.com>
2023-11-24 11:39:32 +0530

    drag and drop update

--- src/components/TodoHomePage.js
+++ src/components/TodoHomePage.js
@@ -1,9 +1,16 @@
 // src/components/HomePage.js
-import React, { useEffect, useState } from 'react';
-import { Button, Container, Table } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import TodoModal from './TodoModal';
-import { deleteTodoAsync, changeActiveFilter, clearCompleted, getTodosAsync } from '../features/todos/todoSlice';
+import React, { useEffect, useState } from "react";
+import { Button, Container, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import TodoModal from "./TodoModal";
+import {
+  deleteTodoAsync,
+  changeActiveFilter,
+  clearCompleted,
+  getTodosAsync,
+  reorderTodos,
+} from "../features/todos/todoSlice";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -18,13 +25,12 @@ const HomePage = () => {
   const handleEditModalClose = () => setShowEditModal(false);
 
   const handleDeleteTodo = (id) => {
-    dispatch(deleteTodoAsync(id)); 
+    dispatch(deleteTodoAsync(id));
   };
 
   useEffect(() => {
     dispatch(getTodosAsync());
-  }, [])
-  
+  }, []);
 
   const handleEditClick = (id) => {
     setEditTodoId(id);
@@ -39,77 +45,109 @@ const HomePage = () => {
     dispatch(clearCompleted());
   };
 
-  const filteredTodos = () => {
-    switch (activeFilter) {
-      case 'all':
-        return Object.values(todos);
-      case 'completed':
-        return Object.values(todos).filter((todo) => todo.completed);
-      case 'active':
-        return Object.values(todos).filter((todo) => !todo.completed);
-      default:
-        return Object.values(todos);
+  const handleDragEnd = (result) => {
+    if (!result.destination) {
+      return;
     }
+
+    const sourceIndex = result.source.index;
+    const destinationIndex = result.destination.index;
+
+    // Reorder the todos
+    const reorderedTodos = Array.from(Object.values(todos));
+    const [removed] = reorderedTodos.splice(sourceIndex, 1);
+    reorderedTodos.splice(destinationIndex, 0, removed);
+
+    // Dispatch the reorder action
+    dispatch(reorderTodos(reorderedTodos));
   };
 
   return (
     <Container className="mt-5">
       <h1>To-Do List</h1>
-      <Button variant="primary" className="mb-3" onClick={() => setShowCreateModal(true)}>
+      <Button
+        variant="primary"
+        className="mb-3"
+        onClick={() => setShowCreateModal(true)}
+      >
         Create Todo
       </Button>
 
       <div className="mb-3">
         <Button
           variant="outline-secondary"
-          className={`mr-2 ${activeFilter === 'all' && 'active'}`}
-          onClick={() => handleFilterChange('all')}
+          className={`mr-2 ${activeFilter === "all" && "active"}`}
+          onClick={() => handleFilterChange("all")}
         >
           All
-        </Button>
-        {' '}
+        </Button>{" "}
         <Button
           variant="outline-success"
-          className={`mr-2 ${activeFilter === 'active' && 'active'}`}
-          onClick={() => handleFilterChange('active')}
+          className={`mr-2 ${activeFilter === "active" && "active"}`}
+          onClick={() => handleFilterChange("active")}
         >
           Active
-        </Button>
-        {' '}
+        </Button>{" "}
         <Button
           variant="outline-danger"
-          className={`mr-2 ${activeFilter === 'completed' && 'active'}`}
-          onClick={() => handleFilterChange('completed')}
+          className={`mr-2 ${activeFilter === "completed" && "active"}`}
+          onClick={() => handleFilterChange("completed")}
         >
           Completed
         </Button>
       </div>
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Todo</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredTodos().map((todo) => (
-            <tr key={todo?.id}>
-              <td>{todo?.id}</td>
-              <td>{todo?.title}</td>
-              <td>
-                <Button variant="info" onClick={() => handleEditClick(todo.id)}>
-                  Edit
-                </Button>{' '}
-                <Button variant="danger" onClick={() => handleDeleteTodo(todo.id)}>
-                  Delete
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="todos">
+          {(provided) => (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Todo</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody ref={provided.innerRef} {...provided.droppableProps}>
+                {Object.values(todos).map((todo, index) => (
+                  <Draggable
+                    key={todo.id}
+                    draggableId={todo.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <tr
+                        key={todo.id}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <td>{todo.id}</td>
+                        <td>{todo.title}</td>
+                        <td>
+                          <Button
+                            variant="info"
+                            onClick={() => handleEditClick(todo.id)}
+                          >
+                            Edit
+                          </Button>{" "}
+                          <Button
+                            variant="danger"
+                            onClick={() => handleDeleteTodo(todo.id)}
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </tbody>
+            </Table>
+          )}
+        </Droppable>
+      </DragDropContext>
 
       <TodoModal show={showCreateModal} handleClose={handleCreateModalClose} />
 
--- src/features/todos/todoSlice.js
+++ src/features/todos/todoSlice.js
@@ -3,156 +3,175 @@ import { createSlice, createAsyncThunk, nanoid } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const api = "https://jsonplaceholder.typicode.com/todos";
-const LOCAL_STORAGE_KEY = 'todos';
-
+const LOCAL_STORAGE_KEY = "todos";
 
 const saveToLocalStorage = (todos) => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
 };
 
 const loadFromLocalStorage = () => {
-    const storedTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
-    return storedTodos ? JSON.parse(storedTodos) : [];
+  const storedTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return storedTodos ? JSON.parse(storedTodos) : [];
 };
 
-export const getTodosAsync = createAsyncThunk("/todos/getTodosAsync", async () => {
+export const getTodosAsync = createAsyncThunk(
+  "/todos/getTodosAsync",
+  async () => {
     try {
-        const res = await axios(api);
-        console.log(res);
-        return res.data;
+      const res = await axios(api);
+      console.log(res);
+      return res.data;
     } catch (error) {
-        throw Error("Error fetching todos");
+      throw Error("Error fetching todos");
     }
-});
+  }
+);
 
-export const toggleTodoAsync = createAsyncThunk("/todos/toggleTodoAsync", async (id) => {
+export const toggleTodoAsync = createAsyncThunk(
+  "/todos/toggleTodoAsync",
+  async (id) => {
     try {
-        await axios.patch(`${api}/${id}`);
-        return id;
+      await axios.patch(`${api}/${id}`);
+      return id;
     } catch (error) {
-        throw Error("Error toggling todo");
+      throw Error("Error toggling todo");
     }
-});
+  }
+);
 
-export const deleteTodoAsync = createAsyncThunk("/todos/deleteTodoAsync", async (id) => {
+export const deleteTodoAsync = createAsyncThunk(
+  "/todos/deleteTodoAsync",
+  async (id) => {
     try {
-        await axios.delete(`${api}/${id}`);
-        return id;
+      await axios.delete(`${api}/${id}`);
+      return id;
     } catch (error) {
-        throw Error("Error deleting todo");
+      throw Error("Error deleting todo");
     }
-});
+  }
+);
 
-export const addTodoAsync = createAsyncThunk("/todos/addTodoAsync", async (data) => {
+export const addTodoAsync = createAsyncThunk(
+  "/todos/addTodoAsync",
+  async (data) => {
     try {
-        const res = await axios.post(`${api}`, data);
-        return res.data;
+      const res = await axios.post(`${api}`, data);
+      return res.data;
     } catch (error) {
-        throw Error("Error adding todo");
+      throw Error("Error adding todo");
     }
-});
+  }
+);
 
-export const editTodoAsync = createAsyncThunk("/todos/editTodoAsync", async ({ id, data }) => {
+export const editTodoAsync = createAsyncThunk(
+  "/todos/editTodoAsync",
+  async ({ id, data }) => {
     try {
-        await axios.put(`${api}/${id}`, data);
-        return { id, data };
+      await axios.put(`${api}/${id}`, data);
+      return { id, data };
     } catch (error) {
-        throw Error("Error editing todo");
+      throw Error("Error editing todo");
     }
-});
+  }
+);
 
 const todosSlice = createSlice({
-    name: "todos",
-    initialState: {
-        items: loadFromLocalStorage(),
-        allIds: Object.keys(loadFromLocalStorage()),
-        activeFilter: "all",
-        getStatus: "idle",
-        addStatus: "idle",
-        error: null,
+  name: "todos",
+  initialState: {
+    items: loadFromLocalStorage(),
+    allIds: Object.keys(loadFromLocalStorage()),
+    activeFilter: "all",
+    getStatus: "idle",
+    addStatus: "idle",
+    error: null,
+  },
+  reducers: {
+    changeActiveFilter: (state, action) => {
+      state.activeFilter = action.payload;
     },
-    reducers: {
-        changeActiveFilter: (state, action) => {
-            state.activeFilter = action.payload;
-        },
-        clearCompleted: (state, action) => {
-            state.items = state.items.filter(item => !item.completed);
-            state.allIds = state.items.map(todo => todo.id);
-        },
+    clearCompleted: (state, action) => {
+      state.items = state.items.filter((item) => !item.completed);
+      state.allIds = state.items.map((todo) => todo.id);
     },
-    extraReducers(builder) {
-        builder
-            .addCase(getTodosAsync.pending, (state) => {
-                state.getStatus = "pending";
-            })
-            .addCase(getTodosAsync.fulfilled, (state, action) => {
-                const todos = action.payload.reduce((acc, todo) => {
-                    acc[todo.id] = todo;
-                    return acc;
-                }, {});
-                state.items = todos;
-                state.allIds = Object.keys(todos);
-                state.getStatus = "succeeded";
-            })
-            .addCase(getTodosAsync.rejected, (state) => {
-                state.getStatus = "failed";
-                state.error = "Error fetching todos";
-            })
-            .addCase(addTodoAsync.pending, (state) => {
-                state.addStatus = "pending";
-            })
-            .addCase(addTodoAsync.fulfilled, (state, action) => {
-                const { id, title } = action.payload;
-                state.items[id] = { id, title, completed: false };
-                state.allIds.push(id);
-                state.addStatus = "succeeded";
-                saveToLocalStorage(state.items);
-            })
-            .addCase(addTodoAsync.rejected, (state) => {
-                state.addStatus = "failed";
-                state.error = "Error adding todo";
-            })
-            .addCase(editTodoAsync.fulfilled, (state, action) => {
-                const { id, data } = action.payload;
-                state.items[id] = { ...state.items[id], ...data };
-                saveToLocalStorage(state.items);
-            })
-            .addCase(editTodoAsync.rejected, (state) => {
-                state.addStatus = "failed";
-                state.error = "Error editing todo";
-            })
-            .addCase(toggleTodoAsync.fulfilled, (state, action) => {
-                const id = action.payload;
-                state.items[id].completed = !state.items[id].completed;
-                saveToLocalStorage(state.items);
-            })
-            .addCase(deleteTodoAsync.fulfilled, (state, action) => {
-                const id = action.payload;
-                delete state.items[id];
-                state.allIds = state.allIds.filter((itemId) => itemId !== id);
-                saveToLocalStorage(state.items);
-            });
-    },
+    reorderTodos: (state, action) => {
+        state.items = action.payload;
+        saveToLocalStorage(state.items);
+      },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getTodosAsync.pending, (state) => {
+        state.getStatus = "pending";
+      })
+      .addCase(getTodosAsync.fulfilled, (state, action) => {
+        const todos = action.payload.reduce((acc, todo) => {
+          acc[todo.id] = todo;
+          return acc;
+        }, {});
+        state.items = todos;
+        state.allIds = Object.keys(todos);
+        state.getStatus = "succeeded";
+      })
+      .addCase(getTodosAsync.rejected, (state) => {
+        state.getStatus = "failed";
+        state.error = "Error fetching todos";
+      })
+      .addCase(addTodoAsync.pending, (state) => {
+        state.addStatus = "pending";
+      })
+      .addCase(addTodoAsync.fulfilled, (state, action) => {
+        const { id, title } = action.payload;
+        state.items[id] = { id, title, completed: false };
+        state.allIds.push(id);
+        state.addStatus = "succeeded";
+        saveToLocalStorage(state.items);
+      })
+      .addCase(addTodoAsync.rejected, (state) => {
+        state.addStatus = "failed";
+        state.error = "Error adding todo";
+      })
+      .addCase(editTodoAsync.fulfilled, (state, action) => {
+        const { id, data } = action.payload;
+        state.items[id] = { ...state.items[id], ...data };
+        saveToLocalStorage(state.items);
+      })
+      .addCase(editTodoAsync.rejected, (state) => {
+        state.addStatus = "failed";
+        state.error = "Error editing todo";
+      })
+      .addCase(toggleTodoAsync.fulfilled, (state, action) => {
+        const id = action.payload;
+        state.items[id].completed = !state.items[id].completed;
+        saveToLocalStorage(state.items);
+      })
+      .addCase(deleteTodoAsync.fulfilled, (state, action) => {
+        const id = action.payload;
+        delete state.items[id];
+        state.allIds = state.allIds.filter((itemId) => itemId !== id);
+        saveToLocalStorage(state.items);
+      });
+  },
 });
 
 export default todosSlice.reducer;
 
-export const { changeActiveFilter, clearCompleted } = todosSlice.actions;
+export const { changeActiveFilter, clearCompleted, reorderTodos } =
+  todosSlice.actions;
 
 export const selectTodos = (state) => Object.values(state.todos.items);
 
 export const selectTodoById = (state, todoId) => state.todos.items[todoId];
 
 export const selectTodosByFilter = (state) => {
-    const { activeFilter, items, allIds } = state.todos;
-    const filteredIds =
-        activeFilter === "all"
-            ? allIds
-            : activeFilter === "completed"
-            ? allIds.filter((id) => items[id].completed)
-            : allIds.filter((id) => !items[id].completed);
+  const { activeFilter, items, allIds } = state.todos;
+  const filteredIds =
+    activeFilter === "all"
+      ? allIds
+      : activeFilter === "completed"
+      ? allIds.filter((id) => items[id].completed)
+      : allIds.filter((id) => !items[id].completed);
 
-    return filteredIds.map((id) => items[id]);
+  return filteredIds.map((id) => items[id]);
 };
 
 export const selectTodosStatus = (state) => state.todos.getStatus;
